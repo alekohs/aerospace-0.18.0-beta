953fe4b3d18adb431c21a32c29e4f5adf1006e16
Alexander Kohkoinen Morin <alexander@alekoh.se>
2025-04-13 13:06:30 +0200

    aerospace: 0.17.1-Beta -> 0.18.0-Beta

--- pkgs/by-name/ae/aerospace/package.nix
+++ pkgs/by-name/ae/aerospace/package.nix
@@ -9,7 +9,7 @@
 
 let
   appName = "AeroSpace.app";
-  version = "0.17.1-Beta";
+  version = "0.18.0-Beta";
 in
 stdenv.mkDerivation {
   pname = "aerospace";
@@ -18,7 +18,7 @@ stdenv.mkDerivation {
 
   src = fetchzip {
     url = "https://github.com/nikitabobko/AeroSpace/releases/download/v${version}/AeroSpace-v${version}.zip";
-    sha256 = "sha256-IMU0s57dpes7Vm2Wv191LwkRgiF+ZIqNWHzrl4a1Pm0=";
+    sha256 = "sha256-S4q6XNoBiAZOZS7cNK4LPTzTZpciNoGAKh1Q5Gzt//o=";
   };
 
   nativeBuildInputs = [ installShellFiles ];
